ce1ec66ccfaf51aee9360a63ba05f15dcb681859
aranajhonny <jhonnyjosearana@gmail.com>
2016-07-02 19:23:36 -0430

    delete database

--- controller/categoriasController.php
+++ controller/categoriasController.php
@@ -1,79 +1,79 @@
 <?php
-	/**
-	* 
-	*/
-	class CategoriasController 
-	{
-		
-		public function __construct(){
-			require_once("model/categoriaModel.php");
-			if (($_SESSION['id_emp']) == "") {
-				header("Location: ".Conectar::ruta()."?controller=login");
-			}
-		}
-		
-		public function index(){
-			require_once("views/layout/template.php");
-			require_once("views/categorias/categorias.php");
-			
-		}
-		public function all(){
-			$c = new categoria();
-			$categorias = $c->get_categorias() ;
-			$data = array();
-			foreach ($categorias as $categoria) {
-				$row = array($categoria['nombre'],
-					$categoria['descripcion'],
-					'<a  data-toggle="tooltip" title="Editar" href="javascript:void(0)" onclick="javascript:editar_categoria('."'".$categoria['id_cat']."'".')"><i class=" btn btn-xs btn-editar
-					glyphicon glyphicon-pencil"></i></a>
-					<a  data-toggle="tooltip" title="Eliminar" href="javascript:void(0)" onclick="javascript:eliminarCategoria('."'".$categoria['id_cat']."'".')" ><i class=" btn btn-xs btn-delete 
-						glyphicon glyphicon-trash"></i></a>');
-				$data[] = $row;
-			}
-			$output = array("data" => $data);
-			echo json_encode($output);			
-		}
+/**
+*
+*/
+class CategoriasController
+{
 
-		public function create(){
-			require_once("views/layout/template.php");
-			require_once("views/categorias/crear_categoria.php");
-			$categoria = new categoria;	
-			if((isset($_SESSION['token'])) && ($_POST['token'] == $_SESSION['token'])){
-				$nombre = $_POST['nombre'];
-				$descripcion = $_POST['descripcion'];
-				$categoria->create_categoria($nombre,$descripcion);
-			}
-		}
-
-		public function getCategoriaById(){
-			$id_cat = $_GET["id_cat"];	
-			$c = new categoria();
-			if (isset($_GET["id_cat"]) and $_GET["id_cat"] != "") {
-				$categoria = $c->get_categoria_by_id($id_cat);
-			}
-		}
-
-		public function update(){
-			$categoria = new categoria;	
-			if((isset($_SESSION['token'])) && ($_POST['token'] == $_SESSION['token'])){
-				$nombre = $_POST['nombre'];
-				$descripcion = $_POST['descripcion'];
-				$id_cat = $_POST['id_cat'];
-				$categoria->update_categoria($nombre,$descripcion,$id_cat);
-			}
-		}
-
-		public function delete(){
-			$id_cat=$_GET["id_cat"];
-			$categoria = new categoria();
-			if (isset($_GET["id_cat"])){
-				$categoria->delete_categoria($id_cat);
-			}else{
-				header("location: ".Conectar::ruta()."?controller=error&action=index");
-				exit();
-			}
+	public function __construct(){
+		require_once("model/categoriaModel.php");
+		if (($_SESSION['id_emp']) == "") {
+			header("Location: ".Conectar::ruta()."?controller=login");
 		}
 	}
 
+	public function index(){
+		require_once("views/layout/template.php");
+		require_once("views/categorias/categorias.php");
 
-	?>
+	}
+	public function all(){
+		$c = new categoria();
+		$categorias = $c->get_categorias() ;
+		$data = array();
+		foreach ($categorias as $categoria) {
+			$row = array($categoria['nombre'],
+			$categoria['descripcion'],
+			'<a  data-toggle="tooltip" title="Editar" href="javascript:void(0)" onclick="javascript:editar_categoria('."'".$categoria['id_cat']."'".')"><i class=" btn btn-xs btn-editar
+			glyphicon glyphicon-pencil"></i></a>
+			<a  data-toggle="tooltip" title="Eliminar" href="javascript:void(0)" onclick="javascript:eliminarCategoria('."'".$categoria['id_cat']."'".')" ><i class=" btn btn-xs btn-delete
+			glyphicon glyphicon-trash"></i></a>');
+			$data[] = $row;
+		}
+		$output = array("data" => $data);
+		echo json_encode($output);
+	}
+
+	public function create(){
+		require_once("views/layout/template.php");
+		require_once("views/categorias/crear_categoria.php");
+		$categoria = new categoria;
+		if((isset($_SESSION['token'])) && ($_POST['token'] == $_SESSION['token'])){
+			$nombre = $_POST['nombre'];
+			$descripcion = $_POST['descripcion'];
+			$categoria->create_categoria($nombre,$descripcion);
+		}
+	}
+
+	public function getCategoriaById(){
+		$id_cat = $_GET["id_cat"];
+		$c = new categoria();
+		if (isset($_GET["id_cat"]) and $_GET["id_cat"] != "") {
+			$categoria = $c->get_categoria_by_id($id_cat);
+		}
+	}
+
+	public function update(){
+		$categoria = new categoria;
+		if((isset($_SESSION['token'])) && ($_POST['token'] == $_SESSION['token'])){
+			$nombre = $_POST['nombre'];
+			$descripcion = $_POST['descripcion'];
+			$id_cat = $_POST['id_cat'];
+			$categoria->update_categoria($nombre,$descripcion,$id_cat);
+		}
+	}
+
+	public function delete(){
+		$id_cat=$_GET["id_cat"];
+		$categoria = new categoria();
+		if (isset($_GET["id_cat"])){
+			$categoria->delete_categoria($id_cat);
+		}else{
+			header("location: ".Conectar::ruta()."?controller=error&action=index");
+			exit();
+		}
+	}
+}
+
+
+?>
--- public/js/app.js
+++ public/js/app.js
@@ -1,55 +1,55 @@
-     (function () {
-        $('#table').DataTable({
-          "paging": true,
-          "lengthChange":true,
-          "searching":true,
-          "ordering": true,
-          "info": true,
-          "autoWidth":true
-        });
-      });
+(function () {
+  $('#table').DataTable({
+    "paging": true,
+    "lengthChange":true,
+    "searching":true,
+    "ordering": true,
+    "info": true,
+    "autoWidth":true
+  });
+});
 
 
 
 
-    function editar_categoria(valor){
-      save_method = 'update';
-    //$('#form')[0].reset(); // reset form on modals
-    $('.form-group').removeClass('has-error'); // clear error class
-    $('.help-block').empty(); // clear error string
+function editar_categoria(valor){
+  save_method = 'update';
+  //$('#form')[0].reset(); // reset form on modals
+  $('.form-group').removeClass('has-error'); // clear error class
+  $('.help-block').empty(); // clear error string
 
-    //Ajax Load data from ajax
-    $.ajax({
-      url : "?controller=categorias&action=getCategoriaById&id_cat=" + valor,
-      type: "GET",
-      dataType: "JSON",
-      success: function(data)
-      {
-        $('[name="id_cat"]').val(data.id_cat);
-        $('[name="nombre"]').val(data.nombre);
-        $('[name="descripcion"]').val(data.descripcion);
-            $('#modal_form1').modal('show'); // show bootstrap modal when complete loaded
-            $('.modal-title').text('Editar categoria'); // Set title to Bootstrap modal title
-
-          },
-          error: function (jqXHR, textStatus, errorThrown)
-          {
-            alert('Error get data from ajax');
-          }
-        });
-  }
-  function eliminarCategoria(identificador){
-    if (confirm("¿ Realmente desea eliminar este registro ?"))
+  //Ajax Load data from ajax
+  $.ajax({
+    url : "?controller=categorias&action=getCategoriaById&id_cat=" + valor,
+    type: "GET",
+    dataType: "JSON",
+    success: function(data)
     {
-     $.get('?controller=categorias&action=delete',{id_cat:identificador},function(data){
+      $('[name="id_cat"]').val(data.id_cat);
+      $('[name="nombre"]').val(data.nombre);
+      $('[name="descripcion"]').val(data.descripcion);
+      $('#modal_form1').modal('show'); // show bootstrap modal when complete loaded
+      $('.modal-title').text('Editar categoria'); // Set title to Bootstrap modal title
+
+    },
+    error: function (jqXHR, textStatus, errorThrown)
+    {
+      alert('Error get data from ajax');
+    }
+  });
+}
+function eliminarCategoria(identificador){
+  if (confirm("¿ Realmente desea eliminar este registro ?"))
+  {
+    $.get('?controller=categorias&action=delete',{id_cat:identificador},function(data){
       recargar();
 
     });
-   }
- }
+  }
+}
 
 
- function guardar_categoria(){
+function guardar_categoria(){
   $.ajax({
     url : "?controller=categorias&action=update",
     type: "POST",
